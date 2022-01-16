e68315891ac682c5ebd3e3ce4e5cab73f808dce0
TranXuanAn2803 <tranxuana2803@gmail.com>
2022-01-16 23:23:16 +0700

    Title
    Description ..........

--- app.js
+++ app.js
@@ -11,22 +11,27 @@ const app = express();
 app.use(cookieParser());
 app.use(flash());
 app.use(session({
-  secret: 'secret',
-  cookie: {maxAge: 60000},
-  resave: false,
-  saveUninitialized: false
+    secret: 'secret',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
 }));
 //routes
 const patient = require("./routes/patients");
 const product = require("./routes/products");
-const userAccount = require("./routes/user");
 const package = require("./routes/package");
 const treatmentPlace = require("./routes/treatmentPlace");
+const userAccount = require("./routes/account");
+const login = require("./routes/login");
+const user = require("./routes/user");
+hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 //hbs
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
-hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+hbs.registerPartials(__dirname + "/views/partials", function(err) {});
 
 //cors
 app.use(cors());
@@ -39,24 +44,24 @@ const { QueryTypes } = require("sequelize");
 const { models } = require("./models");
 const sq = require("./models/index");
 sq.sequelize
-  .authenticate()
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.log(err));
+    .authenticate()
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log(err));
 
 //set routes for server
 app.use("/patient", patient);
-app.use("/product",product);
-app.use("/user",userAccount);
-app.use("/addUserAccount",userAccount);
-app.use("/package",package);
-app.use("/treatmentPlace",treatmentPlace);
+app.use("/product", product);
+app.use("/account", userAccount);
+app.use("/package", package);
+app.use("/treatmentPlace", treatmentPlace);
+app.use("/login", login);
+app.use("/user", user);
 
 app.use("/", (req, res) => {
-  res.render("manager/managerDashboard", {
-    nav: "nav",
-    sidebar: "sidebar",
-    tag: "Patient",
-  });
+    res.render("manager/managerDashboard", {
+        tag: "Patient",
+        sidebar: "admin"
+    });
 });
 
 //do not change
--- models/Services/packageService.js
+++ models/Services/packageService.js
@@ -4,62 +4,73 @@ const sq = require("../../models/index");
 const async = require("hbs/lib/async");
 
 const listPackage = () => {
-  return models.NeccessaryPackage.findAll({ raw: true });
+    return models.NeccessaryPackage.findAll({ raw: true });
 };
 
-const addPackage = async (package,jsonfy)=>{
-  try {
-    await models.NeccessaryPackage.create({
-      name: package.name,
-      list_product: jsonfy,
-      limit_count_package_day: package.limit_day,
-      limit_count_package_week: package.limit_week,
-      limit_count_package_month: package.limit_month,
-    });
-  } catch (err) {
-    console.log(err);
-  }
+const addPackage = async(package, jsonfy) => {
+    try {
+        await models.NeccessaryPackage.create({
+            name: package.name,
+            list_product: jsonfy,
+            limit_count_package_day: package.limit_day,
+            limit_count_package_week: package.limit_week,
+            limit_count_package_month: package.limit_month,
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-const update = async(package,jsonfy)=>{
-  try {
-    await models.NeccessaryPackage.update({
-      name: package.name,
-      list_product: jsonfy,
-      limit_count_package_day: package.limit_day,
-      limit_count_package_week: package.limit_week,
-      limit_count_package_month: package.limit_month,
-    },{
-      where:{
-        id:package.id
-      }
-    });
-  } catch (err) {
-    console.log(err);
-  }
+const update = async(package, jsonfy) => {
+    try {
+        await models.NeccessaryPackage.update({
+            name: package.name,
+            list_product: jsonfy,
+            limit_count_package_day: package.limit_day,
+            limit_count_package_week: package.limit_week,
+            limit_count_package_month: package.limit_month,
+        }, {
+            where: {
+                id: package.id
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-const deletePackage = async(package)=>{
-  try{
-    await models.NeccessaryPackage.destroy({
-      where:{
-        id: package.id
-      }
-    })
-  }catch(err){
-    console.log(err);
-  }
+const deletePackage = async(package) => {
+    try {
+        await models.NeccessaryPackage.destroy({
+            where: {
+                id: package.id
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-const findById = async(package)=>{
-  try{
-    return await models.NeccessaryPackage.findAll({
-      where:{
-        id:package.id
-      }
-    })
-  }catch(err){
-    console.log(err);
-  }
+const findById = async(package) => {
+    try {
+        return await models.NeccessaryPackage.findAll({
+            where: {
+                id: package.id
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }
-module.exports = { listPackage,addPackage,listPackage,deletePackage,findById,update};
+const findById2 = async(id) => {
+    try {
+        return await models.NeccessaryPackage.findAll({
+            where: {
+                id: id
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+module.exports = { listPackage, addPackage, listPackage, deletePackage, findById, update, findById2 };
