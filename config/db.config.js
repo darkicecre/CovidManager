1814f32c6b5b2744d16cc1115fd5faf055bf56ac
tranmanhduc2711 <manhduc2711@gmail.com>
2022-01-21 00:02:20 +0700

    update

--- config/db.config.js
+++ config/db.config.js
@@ -2,11 +2,7 @@ module.exports = {
     user: 'postgres',
     password: '2711',
     host: 'localhost',
-<<<<<<< HEAD
-    port: '5432', //localhost db port
-=======
     port: '5000', //localhost db port
->>>>>>> master
     database: 'CovidManager',
     dialect: 'postgres',
     pool: {
