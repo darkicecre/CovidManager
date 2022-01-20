f714d47e92a0fb6b88f1663fb40723b1c93ef2cd
PhuongUyen0712 <ledoanphuonguyen0712@gmail.com>
2022-01-20 20:20:08 +0700

    Update package 8:19PM

--- controllers/user/packageHistory.js
+++ controllers/user/packageHistory.js
@@ -17,19 +17,19 @@ const list = async(req, res, user) => {
         await Promise.all(pt.map(async(package) => {
             package.list_product = eval(package.list_product);
             const productFirst = await ProductService.findById(package.list_product[0])
-            package.imageMain = productFirst[0].image;
-            if (package.list_product.length > 1) {
-                const productSecond = await ProductService.findById(package.list_product[1]);
-                package.imageSecond = productSecond[0].image;
-            } else {
-                package.imageSecond = "https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png";
-            }
-            if (package.list_product.length > 2) {
-                const productThird = await ProductService.findById(package.list_product[2]);
-                package.imageThird = productThird[0].image;
-            } else {
-                package.imageThird = "https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png";
-            }
+            // package.imageMain = productFirst[0].image;
+            // if (package.list_product.length > 1) {
+            //     const productSecond = await ProductService.findById(package.list_product[1]);
+            //     package.imageSecond = productSecond[0].image;
+            // } else {
+            //     package.imageSecond = "https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png";
+            // }
+            // if (package.list_product.length > 2) {
+            //     const productThird = await ProductService.findById(package.list_product[2]);
+            //     package.imageThird = productThird[0].image;
+            // } else {
+            //     package.imageThird = "https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png";
+            // }
             package.price = 0;
             await Promise.all(package.list_product.map(async(product) => {
                 const item = await ProductService.findById(product);
