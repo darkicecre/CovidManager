258ec3ed6378d4166c2104c5aa69bac6d835ff7d
PhuongUyen0712 <75712660+PhuongUyen0712@users.noreply.github.com>
2022-03-15 18:52:58 +0700

    Update the new user sidebar and add Category

--- models/Services/categoryService.js
+++ models/Services/categoryService.js
@@ -3,23 +3,20 @@ const { QueryTypes } = require("sequelize");
 const sq = require("../../models/index");
 const async = require("hbs/lib/async");
 const Category = require("../Category");
+
 const getListCategory =  () => {
     return models.Category.findAll({raw: true});
 };
-
 
 const addCategory = async (category) => {
   try {
     const t = await models.Category.create({
       name: category.name,
     });
-  
   } catch (err) {
     console.log(err);
   }
 };
 
 
-
-//   module.exports = { getListCategory, addCategory};
 module.exports = { getListCategory, addCategory};
--- models/Services/productService.js
+++ models/Services/productService.js
@@ -2,17 +2,22 @@ const { models } = require("..");
 const { QueryTypes } = require("sequelize");
 const sq = require("../../models/index");
 const async = require("hbs/lib/async");
-async function test() {
-  const t = await models.Patient.findAll({ raw: true });
-  console.log(t);
-  return t;
-}
+
 
 // get all products
+// const listProduct = () => {
+//   return models.Product.findAll({ raw: true });
+// };
 const listProduct = () => {
-  return models.Product.findAll({ raw: true });
+  return models.Product.findAll({ 
+    raw: true,
+    include: [{
+      model: models.Category,
+      as: 'category',
+      attributes: ['name'],
+    }]
+   });
 };
-
 const addProduct = async (product) => {
   try {
     await models.Product.create({
@@ -20,7 +25,8 @@ const addProduct = async (product) => {
       image: product.image,
       note: product.note,
       price: product.price,
-      quantity_unit: product.quantity_unit
+      quantity_unit: product.quantity_unit,
+      category_id: product.category_id,
     });
   } catch (err) {
     console.log(err);
@@ -47,6 +53,32 @@ const findById = async(product)=>{
       }
     })
   }catch(err){
+    console.log(err);
+  }
+}
+
+// Tim san pham dua theo category id, nhung function nay nhan vao category id 
+const findByCatId = async(categoryId)=>{
+  try{
+    return await models.Product.findAll({
+      where:{
+        category_id: categoryId
+      }
+    })
+  } catch(err){
+    console.log(err);
+  }
+}
+
+// Tim san pham dua theo category id, nhung function nay nhan vao product, roi lay category id cua product do
+const findByCateId = async(product)=>{
+  try{
+    return await models.Product.findAll({
+      where:{
+        category_id: product.category_id
+      }
+    })
+  } catch(err){
     console.log(err);
   }
 }
